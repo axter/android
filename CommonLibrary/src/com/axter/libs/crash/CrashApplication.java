fb40d8cccf4879a6b5a496ac44e2e32aa32962d8
axter <applestonedream@163.com>
2015-09-25 14:51:36 +0800

    增加一个异常收集器

---
+++ CommonLibrary/src/com/axter/libs/crash/CrashApplication.java
@@ -0,0 +1,12 @@
+package com.axter.libs.crash;
+
+import android.app.Application;
+
+public class CrashApplication extends Application {
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		CrashHandler crashHandler = CrashHandler.getInstance();
+		crashHandler.init(getApplicationContext());
+	}
+}
