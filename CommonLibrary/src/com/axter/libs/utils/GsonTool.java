ca9eb0cfa9415f45eabc5d90aa8f62d79ba08041
axter <applestonedream@163.com>
2015-12-18 16:08:57 +0800

    优化gson获取 增加获取进程的方法

--- CommonLibrary/src/com/axter/libs/utils/GsonTool.java
+++ CommonLibrary/src/com/axter/libs/utils/GsonTool.java
@@ -1,34 +1,60 @@
 package com.axter.libs.utils;
 
-import java.lang.reflect.Type;
+import android.os.Build;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.lang.ref.SoftReference;
+import java.lang.reflect.Modifier;
+import java.lang.reflect.Type;
 
 /**
- * <p>
- * 使用注意
- * <p>
- * <p>
- * 服务器double可能会返回""字串，所以处理要小心
- * </p>
- * 
+ * <p>使用注意</p>
+ * <p>服务器double可能会返回""字串，所以处理要小心</p>
+ *
  * @author zhaobo
- * 
  */
 public class GsonTool {
+	private static SoftReference<Gson> instance;
+
+	public static Gson getInstance() {
+		if (instance == null || instance.get() == null) {
+			Gson mGson;
+			if (Build.VERSION.SDK_INT >= 23) {
+				GsonBuilder gsonBuilder = new GsonBuilder()
+						.excludeFieldsWithModifiers(
+								Modifier.FINAL,
+								Modifier.TRANSIENT,
+								Modifier.STATIC);
+				mGson = gsonBuilder.create();
+			} else {
+				mGson = new Gson();
+			}
+			instance = new SoftReference(mGson);
+		}
+		return instance.get();
+	}
+
 	/**
 	 * json -> 对象
-	 * 
-	 * @param json
-	 *            json字符串
-	 * @param cls
-	 *            目标数据对象
+	 *
+	 * @param json json字符串
+	 * @param cls  目标数据对象
 	 * @return
 	 */
 	public static <T> T toObject(String json, Class<T> cls) {
 		try {
-			Gson gson = new Gson();
-			return gson.fromJson(json, cls);
+			return getInstance().fromJson(json, cls);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public static Object toObject(String result, Type mType) {
+		try {
+			return getInstance().fromJson(result, mType);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -37,22 +63,10 @@ public class GsonTool {
 
 	public static String toString(Object obj) {
 		try {
-			Gson gson = new Gson();
-			return gson.toJson(obj);
+			return getInstance().toJson(obj);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return "";
 	}
-
-	public static Object toObject(String result, Type mType) {
-		try {
-			Gson gson = new Gson();
-			return gson.fromJson(result, mType);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
-
 }
--- CommonLibrary/src/com/axter/libs/utils/base/AppUtils.java
+++ CommonLibrary/src/com/axter/libs/utils/base/AppUtils.java
@@ -1,5 +1,6 @@
 package com.axter.libs.utils.base;
 
+import android.app.ActivityManager;
 import android.content.Context;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
@@ -19,10 +20,25 @@ import java.util.zip.ZipFile;
  */
 public class AppUtils {
 
+	public static String mChannel;
 	private AppUtils() {
 		/* cannot be instantiated */
 		throw new UnsupportedOperationException("cannot be instantiated");
 
+	}
+
+	/**
+	 * 获取当前进程名
+	 */
+	public static String getCurProcessName(Context context) {
+		int pid = android.os.Process.myPid();
+		ActivityManager activityManager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+		for (ActivityManager.RunningAppProcessInfo appProcess : activityManager.getRunningAppProcesses()) {
+			if (appProcess.pid == pid) {
+				return appProcess.processName;
+			}
+		}
+		return null;
 	}
 
 	/**
@@ -97,7 +113,12 @@ public class AppUtils {
 	 * @param context
 	 * @return
 	 */
-	public static String getChannel(Context context) {
+	public static String getChannel(Context context,String defaultstr) {
+		if (mChannel != null){
+			return mChannel;
+		}
+		if(context == null)
+			return defaultstr;
 		ApplicationInfo appinfo = context.getApplicationInfo();
 		String sourceDir = appinfo.sourceDir;
 		String ret = "";
@@ -127,9 +148,10 @@ public class AppUtils {
 
 		String[] split = ret.split("_");
 		if (split != null && split.length >= 2) {
-			return ret.substring(split[0].length() + 1);
+			mChannel = ret.substring(split[0].length() + 1);
 		} else {
-			return "shanpao";
+			mChannel = defaultstr;
 		}
+		return mChannel;
 	}
 }
